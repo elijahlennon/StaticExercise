321eb69be3a220f6ea1c92a16cb277fa4e876705
Elijah Lennon <elijahlennon1@gmail.com>
2022-06-15 22:28:00 -0500

    completed exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68); 
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20); 
+
+            Console.WriteLine("After conversion:");
+
+            Console.WriteLine($"celsius {celsius}");
+
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
